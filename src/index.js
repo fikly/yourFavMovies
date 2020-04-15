a104e91f68a74b5479ff1920d558fadadb01b908
fikly <fiklyabdullah@gmail.com>
2020-04-15 08:06:09 +0700

    setup environment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './store/ContainerReducer';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/fontawesome/css/all.min.css';
+
+// Route
+import {BrowserRouter , Route, Switch} from 'react-router-dom'
+import routes from './config/routes'
+// End Route
+
+// Pace
+import 'pace-js';
+import 'pace-js/themes/yellow/pace-theme-minimal.css';
+//End Pace
+
+import './assets/scss/global.scss';
+
+const ContainerPage =  React.lazy(()=>import('./views/layouts/Container'))
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={configureStore()}>
+        <BrowserRouter>
+            <Switch>
+                <Suspense fallback={<div>Loading...</div>}>
+                    {routes.map((route, idx) => {
+                        return route.component ? (
+                            <Route
+                                key={idx}
+                                path={route.path}
+                                exact={route.exact}
+                                name={route.name}
+                                render={props => (
+                                    <Suspense fallback={<p>loading ...</p>}>
+                                        <ContainerPage title={route.title} viewPage={route.component} {...props} />
+                                    </Suspense>
+                            )} />
+                        ) : (null);
+                    })}
+                </Suspense>
+            </Switch>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/views/containers/homepage/Page.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Homepage = (props) =>{
+    return(
+        <div className="w-100">
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Homepage
